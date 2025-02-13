98bd5845702be44bbfaf831eb35ff762952796b0
140bpmdubstep <laineprikol@gmail.com>
2025-02-13 18:16:10 +0300

    Needle fits!

--- src/widgets/gauge.rs
+++ src/widgets/gauge.rs
@@ -32,7 +32,7 @@ where
     }
 
     fn draw(&mut self, context: &mut UiContext<'a, D, C>, rect: Rectangle) {
-        let circle = Circle::with_center(rect.center(), rect.size.height / 2).into_styled(
+        let circle = Circle::with_center(Point::new(rect.center().x, rect.center().y - 5), rect.size.height - 10).into_styled(
             PrimitiveStyleBuilder::new()
                 .stroke_color(context.theme.foreground)
                 .stroke_width(GAUGE_STROKE_WIDTH)
@@ -40,27 +40,31 @@ where
         );
 
         let circle_size = circle.primitive.diameter;
+        let center = circle.primitive.center();
+        let min_angle = 40.0;
+        let max_angle = 320.0;
 
         // draw a dashes
-        let mut divisions = rect.size.height / 5;
-        let degrees_per_division = 360.0 / divisions as f32;
-        let desired_angle_per_tick = PI;
-        divisions = (degrees_per_division / desired_angle_per_tick) as u32;
+        let divisions = 3;
+        let total_angle = max_angle - min_angle;
+        let angle_step = total_angle / (divisions - 1) as f32;
 
-        let center = circle.primitive.center();
         let tick_length = circle_size as f32 * 0.1;
         let line_width = GAUGE_STROKE_WIDTH as f32 / 2.0;
 
         for i in 0..divisions {
-            let angle = (i as f32 / (divisions - 1) as f32) * PI * 2.0;
+            let angle = (min_angle + i as f32 * angle_step) + 90.0;
+            let angle_rad = angle.to_radians();
 
-            let start_x = center.x as f32
-                + (circle_size as f32 / 2.0 - line_width / 2.0).floor() * angle.cos();
-            let start_y = center.y as f32
-                + (circle_size as f32 / 2.0 - line_width / 2.0).floor() * angle.sin();
+            let start_x =
+                center.x as f32 + (circle_size as f32 / 2.0 - line_width / 2.0) * angle_rad.cos();
+            let start_y =
+                center.y as f32 + (circle_size as f32 / 2.0 - line_width / 2.0) * angle_rad.sin();
 
-            let end_x = center.x as f32 + (circle_size as f32 / 2.0 - tick_length) * angle.cos();
-            let end_y = center.y as f32 + (circle_size as f32 / 2.0 - tick_length) * angle.sin();
+            let end_x =
+                center.x as f32 + (circle_size as f32 / 2.0 - tick_length) * angle_rad.cos();
+            let end_y =
+                center.y as f32 + (circle_size as f32 / 2.0 - tick_length) * angle_rad.sin();
 
             let _ = Line::new(
                 Point::new((start_x + 0.5) as i32, (start_y + 0.5) as i32),
@@ -75,16 +79,18 @@ where
         // draw a center circle (for needle)
         let _ = Circle::with_center(
             circle.primitive.center(),
-            (circle.primitive.diameter / 10).clamp(2, 8),
+            (circle.primitive.diameter / 10).clamp(2, 4),
         )
         .into_styled(PrimitiveStyle::with_fill(context.theme.foreground))
         .draw(context.draw_target);
 
         // needle
         let needle_width = (circle.primitive.diameter / 10).clamp(1, 2) as f32;
-        let arrow_angle = self.value * 280.0 - 250.0;
+
+        let arrow_angle: f32 =
+            (min_angle + (max_angle - min_angle) * self.value).clamp(0.0, max_angle);
         //println!("{} -> {}", self.value, arrow_angle);
-        let arrow_angle_rad = arrow_angle.to_radians();
+        let arrow_angle_rad = arrow_angle.to_radians() + (PI / 2.0);
         let end_x = center.x as f32
             + (circle_size as f32 / 2.0 + needle_width / 2.0) * arrow_angle_rad.cos();
 
